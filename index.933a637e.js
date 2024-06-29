3bf8c186113360b518a03ca56bfa0c020817c505
MelnykVlad <21vladmel15@gmail.com>
2024-06-29 15:20:22 +0300

    make build

---
+++ index.933a637e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall");e.style.position="relative",t.style.display="flex",t.style.alignItems="center",t.style.justifyContent="center";
+//# sourceMappingURL=index.933a637e.js.map
